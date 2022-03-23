39b736ba78c9f23ccd4f8204c3ed12743e4e2e5c
xx00a <78671259+xx00a@users.noreply.github.com>
2022-03-23 13:34:15 -0400

    Init setup.

--- scripts/jeopardy.js
+++ scripts/jeopardy.js
@@ -5,13 +5,35 @@ let countDownTime = new Date();
 countDownTime.setTime(currentTime.getTime() + 90000);
 
 
+let timer = setInterval( function () {
+
+        let currentTime = new Date().getTime();
+        let delta = currentTime - countDownTime;
+        let secondsRemain = Math.floor(delta / 1000);
+
+        if (secondsRemain < 0)
+        {
+            // valid time
+            document.getElementById("app_countdown").innerHTML = Math.abs(secondsRemain) + " seconds remaining.";
+        }
+        else
+        {
+            clearInterval();
+            document.getElementById("app_countdown").innerHTML = (secondsRemain) + " seconds remaining.";
+
+        }
+
+
+    }
+    ,1000);
+
+
+
 function startGame(name,gameNumber)
 {
     ///
     window.location = "game_do.html";
 }
-
-
 
 
 function countDown()
