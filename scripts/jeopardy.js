b46e565fb362264c4975de4640fd07d61b01b4a4
xx00a <78671259+xx00a@users.noreply.github.com>
2022-03-31 13:37:16 -0400

    Latest updates.

--- scripts/jeopardy.js
+++ scripts/jeopardy.js
@@ -1,15 +1,33 @@
 
-// let's read out cookie to determine what question we are
-let currentQuestion = parseInt( cookieRead('app-game-question') );
-let currentScore = parseInt( cookieRead('app-game-score') );
+
+// global variables and default data for the game session
+let currentGameName = cookieRead('game_name');
+let currentGameNo = parseInt(cookieRead('game_number'));
+let currentQuestion = 0;
+let currentScore = 0;
 let currentAnswer = "";
+let currentSeconds = 0;
 let currentValue = 0;
 let questionsExhausted = false;
 let inPlay = false;
+let questionsComplete = [false,false,false,false,false,false,false,false,false,false,false];
+let questionsScore = [0,0,0,0,0,0,0,0,0,0,0];
+let questionsTime = [0,0,0,0,0,0,0,0,0,0,0];
+let globalScoreboard = [
+    // final score, questions answered, name, date/time, time taken
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+    [0,0,'Name','Date/Time',0],
+];
 
-// question completion tracker
-const questionsComplete = [false,false,false,false,false,false,false,false,false,false,false];
-
+// end of variables
 
 if (isNaN(currentQuestion))
 {
@@ -52,86 +70,175 @@ function countDown()
         if (absSecondsRemain > 0)
         {
             // valid time
+            currentSeconds = 30 - absSecondsRemain;
             document.getElementById("app_msg").innerText = absSecondsRemain + " seconds remain";
         }
         else
         {
             // we submit the answer for them
+            currentSeconds = 30;
             submitQuestion();
         }
 
 }
 
 
-function startGame(name,gameNumber)
-{
+function startGame(name,gameNumber) {
 
-    // draw menus
-    // read cookie to highlight status
+    playQuestion(1);
 
-    // draw opening page
-    // playIntro();
-    cookieSet("intro-played","true");
-
-    // start first availible question (go in a loop to find first quesiton to select)
-    // call playQuestion when ready to go
 }
 
 function playQuestion(questionNo)
 {
-    let activeQuestionNo = 1;
+    let activeQuestionNo = -2;
 
-    // let's determine the next free question to select
     if (isNaN(questionNo))
     {
+        // let's determine the next free question to select
         let qLen = questionsComplete.length;
 
-
-        for (let i = 0; i < qLen; i++) {
-            if (questionsComplete[qLen] === true)
+        for (let i = 1; i < qLen; i++) {
+            if (questionsComplete[i] === false)
             {
-                activeQuestionNo = i + 1;
+                //alert('i = '+i);
+                activeQuestionNo = i;
                 break;
             }
-        }
-    }
-
-    alert(activeQuestionNo);
-
-
-
-
-    if (questionNo < 11)
-    {
-        if (inPlay)
-        {
-            alert("You need to answer the question before choosing a new question.");
-        }
-        else
-        {
-            // reset time variables
-            currentTime = new Date();
-            countDownTime = new Date();
-            countDownTime.setTime(currentTime.getTime() + 30000);
-
-            // update current question
-            currentQuestion = questionNo;
-
-            // startTheGame
-            doQuestion(currentQuestion);
-            cookieSet("app-game-question",currentQuestion);
-
         }
 
     }
     else
     {
-        // let's exit the game
+        // let's confirm if the question has been completed already
+        activeQuestionNo = questionNo;
+
+        if (questionsComplete[activeQuestionNo] === true)
+        {
+            activeQuestionNo = -1;
+        }
+        else
+        {
+            //alert('moving to requested question ('+activeQuestionNo+')');
+
+        }
     }
 
+    if (questionNo > 10) { activeQuestionNo = 11;}
+
+    alert('aq'+activeQuestionNo);
+
+    switch (activeQuestionNo)
+    {
+        case -2:
+            // the game is over
+            finishGame();
+            break;
+        case -1:
+            // they've already answered the question
+            alert("This question has already been answered. Please select another question.");
+            break;
+        default:
+            // show the question
+
+            if (inPlay)
+            {
+                alert("You need to answer the question before choosing a new question.");
+            }
+            else
+            {
+                // reset time variables
+                currentTime = new Date();
+                countDownTime = new Date();
+                countDownTime.setTime(currentTime.getTime() + 30000);
+
+                // update current question
+                currentQuestion = activeQuestionNo;
+
+                // startTheGame
+                doQuestion(currentQuestion);
+                cookieSet("app-game-question",currentQuestion);
+
+            }
+
+            break;
+    }
 }
 
+function finishGame()
+{
+    // let's sum our values up
+    let qCLen = questionsComplete.length;
+    let qSLen = questionsScore.length;
+    let qTLen = questionsTime.length;
+    let sBLen = globalScoreboard.length;
 
+    let sumQuestions = 0;
+    let sumScore = 0;
+    let sumTime = 0;
+
+    // how many questions answered?
+    for (let i = 1; i < qCLen; i++) {
+        if (questionsComplete[i] === true)
+        {
+            sumQuestions = sumQuestions +1;
+        }
+    }
+
+    // tally score?
+    for (let i = 1; i < qSLen; i++) {
+        sumScore = sumScore + questionsScore[i];
+    }
+
+    // tally time
+    for (let i = 1; i < qTLen; i++) {
+            sumTime = sumTime + questionsTime[i];
+    }
+
+    // let's read our master cookies, put them in an array and sort
+    for (let i = 0; i < sBLen; i++) {
+        // final score, questions answered, name, date/time, time taken
+        globalScoreboard[i][0] = parseInt(cookieRead('scoreboard_'+i+'_score'));
+        globalScoreboard[i][1] = parseInt(cookieRead('scoreboard_'+i+'_questions'));
+        globalScoreboard[i][2] = cookieRead('scoreboard_'+i+'_name');
+        globalScoreboard[i][3] = cookieRead('scoreboard_'+i+'_date');
+        globalScoreboard[i][4] = parseInt(cookieRead('scoreboard_'+i+'_time'));
+    }
+
+    // sort results
+    globalScoreboard.sort(function(a,b) {return a[0]-b[0]});
+
+    // let's add our latest result if the lowest value is higher
+    let tempLowestScore = globalScoreboard[0][0];
+
+    if (isNaN(tempLowestScore)) { tempLowestScore = 0; }
+
+    if (tempLowestScore < sumScore)
+    {
+        let winDate = new Date();
+        globalScoreboard[0][0] = sumScore;
+        globalScoreboard[0][1] = sumQuestions;
+        globalScoreboard[0][2] = currentGameName;
+        globalScoreboard[0][3] = winDate.toUTCString();
+        globalScoreboard[0][4] = sumTime;
+    }
+
+    // let's write our scoreboard to... cookies
+    for (let i = 0; i < sBLen; i++) {
+        // final score, questions answered, name, date/time, time taken
+        cookieSet('scoreboard_'+i+'_score',globalScoreboard[i][0]);
+        cookieSet('scoreboard_'+i+'_questions',globalScoreboard[i][1]);
+        cookieSet('scoreboard_'+i+'_name',globalScoreboard[i][2]);
+        cookieSet('scoreboard_'+i+'_date',globalScoreboard[i][3]);
+        cookieSet('scoreboard_'+i+'_time',globalScoreboard[i][4]);
+    }
+
+
+    console.table(globalScoreboard);
+
+
+    //window.location = "game_finish.html";
+}
 
 function doQuestion(questionNo)
 {
@@ -151,7 +258,6 @@ function doQuestion(questionNo)
    // start timer and game
         globalTimer = setInterval( countDown,1000);
         inPlay = true;
-
 
         // call jSon data
         const rawQuestionData ='[{"id":115334,"answer":"a Ring","question":"From Topps, this item that you wear on your finger has a \\"gem\\" made of candy","value":400,"airdate":"2013-07-29T12:00:00.000Z","created_at":"2014-02-14T02:44:56.527Z","updated_at":"2014-02-14T02:44:56.527Z","category_id":15727,"game_id":null,"invalid_count":null,"category":{"id":15727,"title":"candy is dandy","created_at":"2014-02-14T02:44:56.145Z","updated_at":"2014-02-14T02:44:56.145Z","clues_count":10}}]';
@@ -204,7 +310,9 @@ function submitQuestion () {
     let theAnswer = document.getElementsByName('playerResponse')[0].value;
     let currentAnswerTmp = currentAnswer.toLowerCase();
 
-    cookieSet("app-question-"+currentQuestion+"value",theAnswer);
+
+    //cookieSet("app-question-"+currentQuestion+"value",theAnswer);
+
     theAnswer = theAnswer.toLowerCase();
 
     if (theAnswer === currentAnswerTmp)
@@ -241,12 +349,18 @@ function submitQuestion () {
         document.getElementById("gamenext").style.visibility = "visible";
     }
 
-
-
     // update index table
-    questionsComplete[currentQuestion-1] = true;
+//    alert(currentQuestion + ' = '+questionsComplete[currentQuestion]);
+
+
+
+    // let's record our results so we can digest later
+    questionsComplete[currentQuestion] = true;
+    questionsScore[currentQuestion] = currentValue;
+    questionsTime[currentQuestion] = currentSeconds;
 
     inPlay = false;
+
 
 }
 
